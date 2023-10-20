7594c53f01a71e0987e11ae424bd87d8a4d5ca80
Rahidapriya <rahidapriyacse@gmail.com>
2023-10-20 17:53:44 +0600

    implemented No product available message in a brand card

--- src/pages/allproduct/AllProduct.jsx
+++ src/pages/allproduct/AllProduct.jsx
@@ -1,32 +1,41 @@
-
+import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
 import ProductCard from './ProductCard';
 import Navbar from '../../shared/navbar/Navbar';
 import AddSlider from './AddSlider';
- 
- const AllProduct = () => {
-    const products = useLoaderData();
-    const { brand_name } = useParams();
-    // const filteredProducts = products.filter((product) => product.brand_name === brand_name);
-    // console.log(filteredProducts);
 
+const AllProduct = () => {
+  const products = useLoaderData();
+  const { brand_name } = useParams();
+
+  // Check if there are no available products
+  if (products.length === 0) {
     return (
-        <div>
-            <Navbar></Navbar>
-            <AddSlider></AddSlider>
-            <h2 className='text-center text-3xl m-10 font-bold'>Available Product:{products.length}</h2>
+      <div>
+        <Navbar></Navbar>
+        {/* <AddSlider></AddSlider> */}
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+  <h2 className='text-center text-3xl font-bold'>No available products</h2>
+</div>
 
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-10 mx-20'>
-           {
-            products.map(product=><ProductCard key={product._id} product={product}></ProductCard>)
-           }
-
-{/* {filteredProducts.map((product) => (
-          <ProductCard key={product._id} product={product} />
-        ))} */}
-            </div>
-        </div>
+      </div>
     );
- };
- 
- export default AllProduct;
+  }
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <AddSlider></AddSlider>
+      <h2 className='text-center text-3xl m-10 font-bold'>Available Products: {products.length}</h2>
+
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-10 mx-20 my-16'>
+        {products.map((product) => (
+          <ProductCard key={product._id} product={product}></ProductCard>
+        ))
+    }
+      </div>
+    </div>
+  );
+};
+
+export default AllProduct;
