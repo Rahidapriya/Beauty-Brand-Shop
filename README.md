43cf841d9a63fe632e0f51eb20884a073851b09c
Rahidapriya <rahidapriyacse@gmail.com>
2023-10-20 21:20:18 +0600

    added Readme.md file

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-# React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Currently, two official plugins are available:
+# Project Name: Glamer Fixx #
+## Project Category :Cosmetics and Beauty ##
+## Project Features ##
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+  * Users can register on the website and log in via email or Google.
+
+  * Users cannot access most of the private sections (such as Details, Update, and Add products) without   first logging in to the website.
+
+  * Users can view login error and success alerts using toasts and SweetAlert.
+
+  * Users can toggle between the login and registration pages.
+
+  * Users are required to use passwords that contain at least one uppercase letter, one special character, and have a minimum length of 6 characters.
+
+  * User can see specific brand products by clicking on brand card.
+
+  * User can do add to cart .
+
+  * Every user have different "my cart" page.
+
+  * User can add and update products.
+
+  
+ - Live Link : [Click Here](https://brand-shop-a10.web.app/) 
+
--- src/pages/mycart/MyCart.jsx
+++ src/pages/mycart/MyCart.jsx
@@ -5,46 +5,58 @@ import { AuthContext } from "../../providers/AuthProvider";
 import Navbar from "../../shared/navbar/Navbar";
 import Footer from "../../shared/footer/Footer";
 
-
 const MyCart = () => {
-    const {user}=useContext(AuthContext)
-    const loadedCart =useLoaderData();
-    const [myCarts,setMyCarts]= useState(loadedCart);
-    const [filteredProducts,setFilteredProducts]= useState([]);
-    //  const {brand_name}=loadedCart;
-    useEffect(()=>{
-        user && user?.email &&
-         setFilteredProducts (loadedCart.filter((product) => product.userEmail === user?.email));
-    },[user])
-    // console.log(loadedCart);
-    // const {_id, selectedOption,brand_name, name, price, rating, desp, photo }=myCarts;
-    return (
+  const { user } = useContext(AuthContext);
+  const loadedCart = useLoaderData();
+  const [myCarts, setMyCarts] = useState(loadedCart);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    user &&
+      user?.email &&
+      setFilteredProducts(
+        loadedCart.filter((product) => product.userEmail === user?.email)
+      );
+  }, [user]);
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="flex justify-between border-b pb-8 m-20">
+        <h1 className="font-semibold text-2xl">Shopping Cart</h1>
+        <h2 className="font-semibold text-2xl">{filteredProducts.length} Items</h2>
+      </div>
+      {filteredProducts.length > 0 ? ( 
         <div>
-            <Navbar></Navbar>
-            <div className="flex justify-between border-b pb-8 m-20">
-            <h1 className="font-semibold text-2xl">Shopping Cart</h1>
-            <h2 className="font-semibold text-2xl">{filteredProducts.length} Items</h2>
+          <div className="grid lg:grid-cols-2 grid-cols-1 mx-20 my-10 gap-10">
+            {filteredProducts.map((myCart) => (
+              <MyCartCard
+                key={myCart._id}
+                myCart={myCart}
+                myCarts={myCarts}
+                setMyCarts={setMyCarts}
+              ></MyCartCard>
+            ))}
           </div>
-        <div className="grid lg:grid-cols-2 grid-cols-1 mx-20 my-10 gap-10"> 
-        {
-              filteredProducts.map(myCart=><MyCartCard  key={myCart._id}
-                 myCart={myCart}
-                 myCarts={ myCarts}
-                 setMyCarts={setMyCarts}
-                 >
-                   
-                 </MyCartCard>)
-          }
+          <Link to="/">
+            <button className="flex font-semibold text-indigo-600 text-sm mt-10 m-10">
+              <svg
+                className="fill-current mr-2 text-indigo-600 w-4"
+                viewBox="0 0 448 512"
+              >
+                <path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" />
+              </svg>
+              Continue Shopping
+            </button>
+          </Link>
         </div>
-        <Link to='/'>  <button className="flex font-semibold text-indigo-600 text-sm mt-10 m-10">
-            <svg className="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512">
-              <path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" />
-            </svg>
-           Continue Shopping
-          </button></Link>
-            <Footer></Footer> 
+      ) : (
+        <div className="text-center m-10">
+          <p>Your shopping cart is empty.</p>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default MyCart;
